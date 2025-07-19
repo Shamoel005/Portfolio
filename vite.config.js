695595ae0d3e4b21aa4bdd347f06c27e355301b8
Shamoel005 <farooquishamoel@gmail.com>
2025-07-19 15:54:02 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Portfolio/", // 👈 REQUIRED for GitHub Pages
+  base: "/", // 👈 REQUIRED for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
