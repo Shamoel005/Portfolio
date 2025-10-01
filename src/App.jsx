594572c8701e7dc0cafa35b3851e00a0eab52923
Md Shamoel Farooqui <farooquishamoel@gmail.com>
2025-10-01 22:12:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-// import React, { useEffect } from "react";
-// import Navbar from "./components/Navbar";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import Skills from "./components/Skills";
-// import Projects from "./components/Projects";
-// import Experience from "./components/Experience";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-
-// export default function App() {
-//   useEffect(() => {
-//     AOS.init({ duration: 1000, once: true });
-//   }, []);
-
-//   return (
-//     <div
-//   className="min-h-screen bg-cover bg-center text-white font-sans"
-  
-// >
-//   <Navbar />
-//   <Home />
-//   <About />
-//   <Skills />
-//   <Projects />
-//   <Experience />
-//   <Contact />
-//   <Footer />
-// </div>
-
-//   );
-// }
-
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
